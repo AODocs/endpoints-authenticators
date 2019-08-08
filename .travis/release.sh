f8ee45f8573bb06239d30ef99b688a9e93df0e3b
Hermann RANGAMANA <hermann.rangamana@altirnao.com>
2019-08-08 16:44:58 +0200

    Make shell script executable

--- .travis/release.sh
+++ .travis/release.sh
@@ -8,23 +8,22 @@ read CONTINUE_RELEASE
 read -p "Please enter a valid travis token:" TRAVIS_TOKEN
 
 if [ "$CONTINUE_RELEASE" = "Y" ]; then
-  TRAVIS_TOKEN=$(travis token)
   TRAVIS_REQUEST='{
    "request": {
    "message": "Perform Maven Release",
    "branch":"master",
    "config": {
-     "script": ".travis/travis-release.sh"
+     "script": "./.travis/travis-release.sh"
     }
   }}'
 
-  curl -s -X POST \
+  curl -v -s -X POST \
    -H "Content-Type: application/json" \
    -H "Accept: application/json" \
    -H "Travis-API-Version: 3" \
    -H "Authorization: token $TRAVIS_TOKEN" \
    -d "$TRAVIS_REQUEST" \
-   https://api.travis-ci.org/repo/AODocs%2Fendpoints-authenticators/requests
+   https://api.travis-ci.com/repo/AODocs%2Fendpoints-authenticators/requests
 else
   echo "Aborted."
 fi
