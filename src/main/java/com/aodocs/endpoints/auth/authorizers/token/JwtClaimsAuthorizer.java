c5da737dcd065209ed6818a5a7088c147941ae81
Hermann RANGAMANA <hermann.rangamana@altirnao.com>
2022-01-21 08:22:08 +0100

    Offer generic method to query claims in JWT token
    The class AuthInfo already contains some information from the token.
    But for JWT token, it lacks method to query arbitrary claim from
    the token: a new method was added then 'getClaims()' than enables
    querying arbitrary claim, using a jsonp-style key.

--- src/main/java/com/aodocs/endpoints/auth/authorizers/token/JwtClaimsAuthorizer.java
+++ src/main/java/com/aodocs/endpoints/auth/authorizers/token/JwtClaimsAuthorizer.java
@@ -19,32 +19,24 @@
  */
 package com.aodocs.endpoints.auth.authorizers.token;
 
-import java.io.IOException;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
 import java.util.function.BiPredicate;
 import java.util.function.Predicate;
-import java.util.logging.Level;
 import java.util.stream.Stream;
 
 import javax.servlet.http.HttpServletRequest;
 
 import lombok.AccessLevel;
 import lombok.Getter;
-import lombok.SneakyThrows;
 import lombok.extern.java.Log;
 
+import com.aodocs.endpoints.auth.AuthInfo;
 import com.aodocs.endpoints.auth.ExtendedUser;
-import com.fasterxml.jackson.annotation.JsonUnwrapped;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.JsonNodeType;
-import com.google.api.client.auth.openidconnect.IdToken;
 import com.google.api.server.spi.config.model.ApiMethodConfig;
 import com.google.common.annotations.VisibleForTesting;
-import com.google.common.base.Preconditions;
 
 /**
  * Checks if the token is a JWT token, and checks claims from this token.
@@ -73,61 +65,28 @@ public abstract class JwtClaimsAuthorizer extends JwtOnlyAuthorizer {
         return claims;
     }
     
-    //mus be overridden to set the property name
+    //must be overridden to set the property name
     public JwtClaimsAuthorizer setClaims(Map<String, Object> claims) {
         this.claims.putAll(claims);
         return this;
     }
     
     @Override
-    @SneakyThrows(IOException.class)
     public AuthorizationResult isAuthorized(ExtendedUser extendedUser, ApiMethodConfig apiMethodConfig, HttpServletRequest request) {
         AuthorizationResult result = super.isAuthorized(extendedUser, apiMethodConfig, request);
         if (!result.isAuthorized()) {
             return result;
         }
-        return new AuthorizationResult(checkJwtClaims(getIdTokenPayload(extendedUser)));
-    }
-    
-    private IdToken.Payload getIdTokenPayload(ExtendedUser extendedUser) {
-        Object rawTokenInfo = extendedUser.getAuthInfo().getRawTokenInfo();
-        Preconditions.checkState(rawTokenInfo instanceof IdToken, "Auth type is JWT, but token info is not an ID token");
-        return ((IdToken) rawTokenInfo).getPayload();
+        return new AuthorizationResult(checkJwtClaims(extendedUser.getAuthInfo()));
     }
     
     @VisibleForTesting
-    boolean checkJwtClaims(IdToken.Payload payload) throws JsonProcessingException {
-        //convert to JsonNode to be able to use JsonPointer syntax
-        JsonNode asJsonTree = new ObjectMapper().readTree(payload.toString());
-        return combiner.test(claims.entrySet().stream(), entry -> checkClaim(asJsonTree, entry.getKey(), entry.getValue()));
+    boolean checkJwtClaims(AuthInfo authInfo) {
+        return combiner.test(claims.entrySet().stream(), entry -> checkClaim(authInfo.getClaims(), entry.getKey(), entry.getValue()));
     }
     
-    private boolean checkClaim(JsonNode payload, String key, Object value) {
-        //make the key a valid json pointer
-        if (!key.startsWith("/")) {
-            key = "/" + key;
-        }
-        JsonNode node = payload.at(key);
-        JsonNodeType nodeType = node.getNodeType();
-        switch (nodeType) {
-            case BOOLEAN:
-                return node.isBoolean() && value.equals(node.asBoolean());
-            case NUMBER:
-                return node.isNumber() && value instanceof Number && ((Number) value).longValue() == node.asLong();
-            case STRING:
-                return node.isTextual() && value.equals(node.asText());
-            case NULL:
-            case MISSING:
-                return value == null;
-            case OBJECT:
-            case POJO:
-            case ARRAY:
-            case BINARY:
-                log.log(Level.WARNING,
-                        "Cannot compare node of type {0} at {1} with ''{2}'' (node value is ''{3}'')",
-                        new Object[] {nodeType, key, value, node});
-        }
-        return false;
+    private boolean checkClaim(AuthInfo.Claims claims, String key, Object value) {
+        Optional<Object> claim = claims.get(key);
+        return claim == null ? false : claim.map(claimValue -> Objects.equals(claimValue, value)).orElse(value == null);
     }
-    
 }
